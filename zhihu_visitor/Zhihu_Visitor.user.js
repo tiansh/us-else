8335700f3eb969413cbca986d2e5025aad093e15
tiansh <tiansheng111@gmail.com>
2015-06-14 14:49:44 +1000

    3.1 fix updateURL

--- zhihu_visitor/Zhihu_Visitor.user.js
+++ zhihu_visitor/Zhihu_Visitor.user.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @namespace         https://github.com/tiansh
 // @name              Zhihu Visitor
 // @name:en           Zhihu Visitor
@@ -9,11 +9,11 @@
 // @description:zh    知乎免登录查看回答全文并显示更多回答
 // @description:zh-CN 知乎免登录查看回答全文并显示更多回答
 // @include           http://www.zhihu.com/*
-// @updateURL         http://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.meta.js
-// @downloadURL       http://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.user.js
-// @homepageURL       http://tiansh.github.io/us-else/zhihu_visitor/
+// @updateURL         https://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.meta.js
+// @downloadURL       https://tiansh.github.io/us-else/zhihu_visitor/Zhihu_Visitor.user.js
+// @homepageURL       https://tiansh.github.io/us-else/zhihu_visitor/
 // @supportURL        https://github.com/tiansh/us-else/issues/
-// @version           3.0
+// @version           3.1
 // @grant             GM_addStyle
 // @grant             GM_xmlhttpRequest
 // ==/UserScript==
